3875e6c6ebde52a09391ee08c6fe247a0600f9e8
Eivind Uggedal <eu@redflavor.com>
2009-03-25 17:05:09 +0100

    Should now be setuptools compatible. Pip here I come!

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-positions',
+    version='0.1.0',
+    description='A Django field for custom model ordering.',
+    author='Joel Watts',
+    author_email='joel@joelwatts.com',
+    url='http://code.google.com/p/django-positions/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
