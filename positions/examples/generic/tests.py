d675741db03a16f3150ddc8233cffe370977f2f0
Jacob Smullyan <smulloni@cecily.smullyan.org>
2009-11-14 23:31:25 -0500

    some additional tests

--- positions/examples/generic/tests.py
+++ positions/examples/generic/tests.py
@@ -5,13 +5,14 @@ from positions.examples.generic.models import GenericThing
 
 tests="""
 >>> l = List.objects.create(name='To Do')
+>>> ct=ContentType.objects.get_for_model(l)
 >>> t1=GenericThing.objects.create(name="First Generic Thing",
 ...                                object_id=l.pk,
-...                                content_type=ContentType.objects.get_for_model(l))
+...                                content_type=ct)
 
 >>> t2=GenericThing.objects.create(name="Second Generic Thing",
 ...                                object_id=l.pk,
-...                                content_type=ContentType.objects.get_for_model(l))
+...                                content_type=ct)
 >>> t1.position
 0
 >>> t2.position
@@ -24,5 +25,22 @@ tests="""
 >>> t2=GenericThing.objects.get(pk=2)
 >>> t2.position
 0
+>>> t1.delete()
+
+>>> GenericThing.objects.filter(object_id=l.pk, content_type=ct).values_list('name', 'position').order_by('position')
+[(u'Second Generic Thing', 0)]
+>>> t3=GenericThing.objects.create(object_id=l.pk, content_type=ct, name='Mr. None')
+>>> t3.save()
+>>> t3.position
+1
+>>> t4=GenericThing.objects.create(object_id=l.pk, content_type=ct, name='Mrs. None')
+>>> t4.position
+2
+>>> t4.position=-2
+>>> t4.save()
+>>> t4.position
+1
+>>> GenericThing.objects.order_by('position').values_list('name', flat=True)
+[u'Second Generic Thing', u'Mrs. None', u'Mr. None']
 """
 __test__ = {'tests': tests}
