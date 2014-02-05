4c5523f06d9aac06c0f716339e8a809fd66c96f2
Joel Watts <joel@joelwatts.com>
2014-02-04 20:34:13 -0600

    Added Django to install_requires.

--- setup.py
+++ setup.py
@@ -19,5 +19,8 @@ setup(
     ],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['setuptools'],
+    install_requires=[
+        'Django',
+        'setuptools'
+    ],
 )
