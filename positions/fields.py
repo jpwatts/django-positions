jpwatts/django-positions

commit 9329e49f63197dba4228078684b00d456e4ec16c

Don't pollute the model namespace.
Track collection change status on the field instance instead of on
the model instance. I doubt people are using `collection_changed`
as a field bane, but it's better to avoid setting attributes on the
model instance if it can be avoided.

positions/fields.py
@@ -38,6 +38,7 @@ class PositionField(models.IntegerField):
             collection = (collection,)
         self.collection = collection
         self.parent_link = parent_link
+        self._collection_changed =  None
 
     def contribute_to_class(self, cls, name):
         super(PositionField, self).contribute_to_class(cls, name)
@@ -73,7 +74,7 @@ class PositionField(models.IntegerField):
         if not collection_changed:
             previous_instance = None
 
-        setattr(model_instance, 'collection_changed', collection_changed)
+        self._collection_changed = collection_changed
         if collection_changed:
             self.remove_from_collection(previous_instance)
 
@@ -210,7 +211,8 @@ class PositionField(models.IntegerField):
         setattr(instance, '_next_sibling_pk', None)
 
     def update_on_save(self, sender, instance, created, **kwargs):
-        collection_changed = getattr(instance, 'collection_changed', False)
+        collection_changed = self._collection_changed
+        self._collection_changed = None
 
         current, updated = getattr(instance, self.get_cache_name())
 

commit 010e3e9fcdbd24242ce2c5e6d866e03ad7ebd888

Fix importing fixtures
This was broken since commit:

  9329e49 Don't pollute the model namespace.

This change fixes issue #29.

positions/fields.py
@@ -224,7 +224,7 @@ class PositionField(models.IntegerField):
 
         current, updated = getattr(instance, self.get_cache_name())
 
-        if updated is None and collection_changed == False:
+        if updated is None and not collection_changed:
             return None
 
         queryset = self.get_collection(instance).exclude(pk=instance.pk)