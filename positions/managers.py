8c25995d1c9324c146a021815cb45e69fa83df6d
Areth Foster-Webster <arethfw@nvbots.com>
2015-02-16 14:09:12 -0500

    Add a logger.

--- positions/managers.py
+++ positions/managers.py
@@ -4,6 +4,8 @@ from django.db.models.signals import post_save
 
 from positions.fields import PositionField
 
+import logging
+logger = logging.getLogger(__name__)
 
 class PositionQuerySet(QuerySet):
     def __init__(self, model=None, query=None, using=None, position_field_name='position', hints=None):
