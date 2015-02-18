7d7f3c520d662aba82abf36804da5f42344ca271
Areth Foster-Webster <arethfw@nvbots.com>
2015-02-18 14:28:32 -0500

    Update settings files so they don't throw warnings in Django 1.7. Add specific settings files for postgres and sqlite.

--- positions/examples/settings_postgres.py
+++ positions/examples/settings_postgres.py
@@ -68,7 +68,12 @@ WSGI_APPLICATION = 'local.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_positions',
+        'USER': 'django_positions',
+        'PASSWORD': 'django_positions',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
